30071cfbd44e6d37274ebb72b6dbeac7fb48d605
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-02-20 21:52:50 +0100

    feat: added custom exception

---
+++ src/main/java/com/lsadf/yaproc/exception/UnsupportedFileFormatException.java
@@ -0,0 +1,7 @@
+package com.lsadf.yaproc.exception;
+
+public class UnsupportedFileFormatException extends RuntimeException {
+  public UnsupportedFileFormatException(String message) {
+    super(message);
+  }
+}
